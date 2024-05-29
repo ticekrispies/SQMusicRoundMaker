e9a2b3164fa134ef72137ea050c35197f483439a
Lux-Ferre <Lux-Ferre@users.noreply.github.com>
2024-05-29 15:07:18 +0100

    flask endpoint to get spotify client id

--- javascript/dev_server.py
+++ javascript/dev_server.py
@@ -13,3 +13,11 @@ app = Flask(__name__)
 @app.route("/")
 def main_page():
     return render_template("sq-music.html")
+
+
+@app.get("/clientid")
+def get_client_id():
+    client_data = {
+        "client_id": os.environ["SPOTIFY_CLIENT_ID"]
+    }
+    return json.dumps(client_data)
